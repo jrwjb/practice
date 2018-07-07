32b6c69f041a266cc273fa42447bff6fa24d7bde
jrwjb <36021958+jrwjb@users.noreply.github.com>
2018-07-07 19:54:52 +0800

    Delete panddjie6.py

--- codefile/panddjie6.py
+++
@@ -1,9 +0,0 @@
-f_A=open(r'C:\Users\wjb\Desktop\6\A')
-f_B=open(r'C:\Users\wjb\Desktop\6\B')
-onlyA=open(r'C:\Users\wjb\Desktop\6\onlyA','w')
-onlyB=open(r'C:\Users\wjb\Desktop\6\onlyB','w')
-bothAB=open(r'C:\Users\wjb\Desktop\6\bothAB','w')
-for i in f_A:
-    if i in f_B:
-        print(i)
-        bothAB.write(i)
