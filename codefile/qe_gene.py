b5b1747bc1487a7e357eb5d3f061edf945b37d39
jrwjb <jrwjb@sina.com>
2019-06-06 16:10:54 +0800

    add genename

--- codefile/qe_gene.py
+++ codefile/qe_gene.py
@@ -25,22 +25,25 @@ def fn_timer(function):
 
 def gene_name():
     gene_dict = {}
-    with open('E:\\Project\\Protein\\QE\R20190300409\\id.txt') as f:
+    # protein_list, gene_list = [], []
+    with open('C:\\Users\\jbwang\\Desktop\\R20190500644-SX1\\id.txt') as f:
         for i in f:
             protein = i.split('|')[1]
+            # protein_list.append(protein)
             if re.search('GN=\S+', i):
                 gene = re.search('GN=\S+', i).group().split('=')[1]
             else:
                 gene = ''
             gene_dict[protein] = gene
+            # gene_list.append(gene)
             # f2.write(f'{protein},{gene}\n')
+
     return gene_dict
 
-# go = pd.read_excel('E:\\Project\\Protein\\QE\R20190300409\\GO.xlsx')
 @fn_timer
 def write():
-    go = xlrd.open_workbook('E:\\Project\\Protein\\QE\R20190300409\\GO.xlsx')
-    go_table = go.sheet_by_index(8)
+    go = xlrd.open_workbook('C:\\Users\\jbwang\\Desktop\\R20190500644-SX1\\GO.xlsx')
+    go_table = go.sheet_by_index(0)
 
     wb = Workbook()
     ws = wb.active
@@ -52,12 +55,44 @@ def write():
         proteinID = line[0]
         # # print(proteinID)
         geneName = gene_name().get(proteinID)
-        print(geneName)
-        # line.append(geneName)
-        # ws.append(line)
+        # print(geneName)
+        line.append(geneName)
+        ws.append(line)
 
-    wb.save('E:\\Project\\Protein\\QE\R20190300409\\GO2.xlsx')
+    wb.save('C:\\Users\\jbwang\\Desktop\\R20190500644-SX1\\GO2.xlsx')
+
+@fn_timer
+def write2():
+    ## 字典构建dataframe
+    df = pd.DataFrame.from_dict(gene_name(), orient='index')
+    df.columns = ['GeneName']
+    go = pd.read_excel('C:\\Users\\jbwang\\Desktop\\R20190500644-SX1\\GO.xlsx', None, index_col=0)
+    # 写入多个sheet
+    writer = pd.ExcelWriter('C:\\Users\\jbwang\\Desktop\\R20190500644-SX1\\报告及附件\\GO.xlsx')
+    for k in go.keys():
+        data = go[k]
+        #直接使用join
+        new_df = data.join(df)
+        newcol = ['GeneName']
+        newcol.extend(data.columns.tolist())
+        new_df = new_df[newcol]
+        # new_df.to_excel('C:\\Users\\jbwang\\Desktop\\R20190500644-SX1\\GO3.xlsx')
+        new_df.to_excel(writer, sheet_name=k)
+    writer.save()
+    writer.close()
+
+    #########先按蛋白筛选基因名
+    # gene_df = df.loc[go.index]
+    # ##concat方法
+    # # new_df = pd.concat([gene_df, go], sort=False, axis=1)
+    # # new_df.to_excel('C:\\Users\\jbwang\\Desktop\\R20190500644-SX1\\GO3.xlsx')
+    # # # print()
+    # # ## join方法
+    # new_df = gene_df.join(go)
+    # new_df.to_excel('C:\\Users\\jbwang\\Desktop\\R20190500644-SX1\\GO3.xlsx')
+    # # print()
 
 
 if __name__ == '__main__':
-    write()
+    # write()
+    write2()
