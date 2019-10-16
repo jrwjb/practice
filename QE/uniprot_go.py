dbccbc00c8d25eaa91bd938cebee76dd89a9a249
jrwjb <jrwjb@sina.com>
2019-10-16 14:14:43 +0800

    add geneid & genename

--- QE/uniprot_go.py
+++ QE/uniprot_go.py
@@ -48,7 +48,13 @@ class Spider():
                 tmp = [child.attrib.get('value') for child in root.iter(tag='{http://uniprot.org/uniprot}property') if child.attrib.get('type') == 'term']
                 category = [i.split(':')[0] for i in tmp]
                 goterm = [i.split(':')[1] for i in tmp]
-                proname = ''.join([child.text for child in root.iter(tag='{http://uniprot.org/uniprot}fullName')])
+                proname = ''.join([child.text for child in root.iter(tag='{http://uniprot.org/uniprot}fullName')][0])
+                geneid = ''.join([child.attrib.get('id') for child in root.iter(tag='{http://uniprot.org/uniprot}dbReference') if child.attrib.get('type') == 'GeneID'])
+                genename = ''.join([child.text for child in root.iter(tag='{http://uniprot.org/uniprot}name') if child.attrib.get('type') == 'primary'])
+
+                # proname = None if len(proname) == 0 else proname
+                # geneid = None if len(geneid) == 0 else geneid
+                # genename = None if len(genename) == 0 else genename
 
                 bp = set([f'{goterm[k]} [{goid[k]}]' if v == 'P' else '-' for k, v in enumerate(category)])
                 mf = set([f'{goterm[k]} [{goid[k]}]' if v == 'F' else '-' for k, v in enumerate(category)])
@@ -59,18 +65,18 @@ class Spider():
                 all_go.extend(cc)
 
                 if len(goid) == 0:
-                    out = f'{proid}\t{proname}\t-\t-\t-\t-'.split('\t')
+                    out = f'{proid}\t{proname}\t{geneid}\t{genename}\t-\t-\t-\t-'.split('\t')
                 else:
                     bp = ';'.join(bp)
                     mf = ';'.join(mf)
                     cc = ';'.join(cc)
                     all_go = ';'.join(all_go)
-                    out = f'{proid}\t{proname}\t{bp}\t{mf}\t{cc}\t{all_go}'.split('\t')
+                    out = f'{proid}\t{proname}\t{geneid}\t{genename}\t{bp}\t{mf}\t{cc}\t{all_go}'.split('\t')
                 self.data.append(out)
             except Exception as e:
-                out = f'{proid}\t-\t-\t-\t-\t-'.split('\t')
+                out = f'{proid}\t-\t-\t-\t-\t-\t-\t-'.split('\t')
                 self.data.append(out)
-                # print(proid, e)
+                print(e)
     # print(get_info('A1YZ34'))
     @fn_timer
     def main(self):
@@ -90,7 +96,7 @@ class Spider():
                     t.join()
                 ws = wb.create_sheet(d, i)
                 i += 1
-                ws.append(['ProteinID', 'ProteinName', 'BP', 'MF', 'CC', 'GO_Term'])
+                ws.append(['ProteinID', 'ProteinName', 'GeneID', 'GeneName', 'BP', 'MF', 'CC', 'GO_Term'])
                 for j in self.data:
                     # print(j)
                     ws.append(j)
